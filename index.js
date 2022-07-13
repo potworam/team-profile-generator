2cfe7f96eece67266380ca1bf4cfe6c067ff1fd6
Potwora <matthewpotwora@yahoo.com>
2022-07-13 15:21:56 -0700

    addedinquirer prompts

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
+const Employee = require('./lib/Employee.js');
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+
+const questions=[
+    {
+        type: 'input',
+        message: 'Enter your name',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'Enter your ID',
+        name: 'identification',
+    },
+    {
+        type: 'input',
+        message: 'Enter your Email',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'Enter your office number',
+        name: 'officeNumber',
+    },
+    {
+        type: 'input',
+        message: 'Enter your github',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Enter your school',
+        name: 'school',
+    },
+    {
+        type:'list',
+        name: 'role',
+        message: 'What is your role',
+        choices: [ 'Manager', 'Engineer', 'Intern']
+    },
+    //need to add function to add others and loop questions
+]
+ //need to add function to add page template or assign inquirer variables to the right place
+async function init(questions) {
+    let answers = await inquirer.prompt(questions)
+    console.log(answers)
+    await writeToFile("./index.html",answers)
+   return answers
+
+}
+async function app() {
+    await init(questions);
+}
+app();
--- src/page-template.js
+++ src/page-template.js
@@ -1,6 +1,58 @@
 //page with all cards go here
 const Employee = require('../lib/Employee.js');
 
+const generateTeam = team => {
 
-
-
+    const generateManager = manager => {
+        return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${manager.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>
+    </div>
+    `;
+    };
+    const generateEngineer = Engineer => {
+        return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${Engineer.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${Engineer.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${Engineer.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${Engineer.getEmail()}">${Engineer.getEmail()}</a></li>
+                <li class="list-group-item">Github: <a href="https://github.com/${Engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+            </ul>
+        </div>
+    </div>
+    `;
+    };
+    const generateIntern = Intern => {
+        return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${Intern.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${Intern.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${Intern.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${Intern.getEmail()}">${Intern.getEmail()}</a></li>
+                <li class="list-group-item">School: ${Intern.getSchool()}</li>
+            </ul>
+        </div>
+    </div>
+    `;
+    };
+}
+module.exports={generateTeam}
